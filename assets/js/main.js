0665bd6ccb2d1e2c89476b5da8dfe71433e6325b
Stephen Palazzo <stephencpalazzo@gmail.com>
2024-10-12 18:57:30 -0400

    Add hubs

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,36 +6,44 @@ const aboutMeNav = document.querySelector('#about-me-nav');
 const projectsNav = document.querySelector('#projects-nav');
 const contactNav = document.querySelector('#contact-nav');
 
-menuBtn.onclick = () => {
-  navbar.classList.add('show');
-  menuBtn.classList.add('hide');
-  body.classList.add('disabled');
-};
-cancelBtn.onclick = () => {
-  body.classList.remove('disabled');
-  navbar.classList.remove('show');
-  menuBtn.classList.remove('hide');
-};
-aboutMeNav.onclick = () => {
-  body.classList.remove('disabled');
-  navbar.classList.remove('show');
-  menuBtn.classList.remove('hide');
-};
-projectsNav.onclick = () => {
-  body.classList.remove('disabled');
-  navbar.classList.remove('show');
-  menuBtn.classList.remove('hide');
-};
-contactNav.onclick = () => {
-  body.classList.remove('disabled');
-  navbar.classList.remove('show');
-  menuBtn.classList.remove('hide');
-};
+// menuBtn.onclick = () => {
+//   navbar.classList.add('show');
+//   menuBtn.classList.add('hide');
+//   body.classList.add('disabled');
+// };
+// cancelBtn.onclick = () => {
+//   body.classList.remove('disabled');
+//   navbar.classList.remove('show');
+//   menuBtn.classList.remove('hide');
+// };
+// aboutMeNav.onclick = () => {
+//   body.classList.remove('disabled');
+//   navbar.classList.remove('show');
+//   menuBtn.classList.remove('hide');
+// };
+// projectsNav.onclick = () => {
+//   body.classList.remove('disabled');
+//   navbar.classList.remove('show');
+//   menuBtn.classList.remove('hide');
+// };
+// contactNav.onclick = () => {
+//   body.classList.remove('disabled');
+//   navbar.classList.remove('show');
+//   menuBtn.classList.remove('hide');
+// };
 window.onscroll = () => {
   this.scrollY > 20
     ? navbar.classList.add('sticky')
     : navbar.classList.remove('sticky');
 };
+
+// Copy Post
+function copyPost(post) {
+  navigator.clipboard.writeText(post);
+
+  var copyAlertPost = document.getElementById('copy-alert-post');
+  copyAlertPost.innerHTML = 'Copied!';
+}
 
 // Copy Email
 function copyEmail() {
